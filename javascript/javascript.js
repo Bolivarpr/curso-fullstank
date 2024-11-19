f034113a42613de80ef690d2871aee771a996cfd
Marcelo <dennermarcelopereira@gmail.com>
2024-11-19 20:52:17 -0300

    primeiro exercício de variáveis

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -12,7 +12,7 @@ console.log('Me chamo', nome, 'tenho', idade, 'anos', 'e moro no', país)
 console.log('eu conheço o', nome1, 'da internet')
 console.log(país, 'é um dos países maravilhosos do mundo.')
 
-/* trabalhando com const */
+/* trabalhando com const */ 
 
 const p = 8;
 const s = 12;
@@ -25,4 +25,16 @@ const adicao = 8;
 console.log(typeof nome2)
 console.log(typeof adicao)
 
-/*----------- */
+/*exercício de variável
+
+const nome3 = 'Luiz Otávio';
+const sobrenome = 'Miranda';
+const idade1 = 30;
+const peso = 93;
+const altura = 1.75;
+let imc = peso / (altura * altura);
+let anoNascimento = 2024 - idade1;
+
+console.log(nome3, sobrenome, 'tem', idade1, 'anos pesa', peso, 'kg tem', altura, 'de altura e seu IMC é de', imc, 'seu ano de nascimento é', anoNascimento)
+
+/*----------------*/
