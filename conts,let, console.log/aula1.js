d94d63eda74734f8cf2a397f8281018104faa33d
Marcelo <dennermarcelopereira@gmail.com>
2024-11-20 17:01:50 -0300

    aprendendo dados primitivos em js

---
+++ conts,let, console.log/aula1.js
@@ -0,0 +1,38 @@
+// mostrar na tela em console em inspecionar
+console.log
+('Meu nome é "Marcelo". Estou aprendendo Javascript ás', 17, 'da tarde do dia 17/11/24' )
+
+/* Trabalahndo com variáveis*/
+let nome = 'Marcelo';
+let nome1 = 'amenic';
+var idade = '31';
+const país = 'Brasil';
+
+console.log('Me chamo', nome, 'tenho', idade, 'anos', 'e moro no', país)
+console.log('eu conheço o', nome1, 'da internet')
+console.log(país, 'é um dos países maravilhosos do mundo.')
+
+/* trabalhando com const */ 
+
+const p = 8;
+const s = 12;
+const resultado = p + s;
+
+console.log( resultado)
+
+const nome2 = 'joão';
+const adicao = 8;
+console.log(typeof nome2)
+console.log(typeof adicao)
+
+/*exercício de variável*/
+
+const nome3 = 'Luiz Otávio';
+const sobrenome = 'Miranda';
+const idade1 = 30;
+const peso = 93;
+const altura = 1.75;
+let imc = peso / (altura * altura);
+let anoNascimento = 2024 - idade1;
+
+console.log(nome3, sobrenome, 'tem', idade1, 'anos pesa', peso, 'kg tem', altura, 'de altura e seu IMC é de', imc, 'seu ano de nascimento é', anoNascimento)
