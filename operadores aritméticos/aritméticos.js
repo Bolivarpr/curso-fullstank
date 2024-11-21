d5eb03894bc0ee383aca5738cbc0c9b26d87fe02
Marcelo <dennermarcelopereira@gmail.com>
2024-11-20 22:48:29 -0300

    operadores aritméticos

---
+++ operadores aritméticos/aritméticos.js
@@ -0,0 +1,38 @@
+/*aritméticos
+
+ordem dos
+aritméticos 
+()
+**
+* / %
+* -
+
+^ concatenação, ** pontenciação, % resto da divisão
+*/
+
+/* operadores de incremento e decremento*/
+const num1 = 8;
+const num2 = 5;
+const num3 = 6;
+console.log(num1 + num2 * num3);
+
+/*
+let contador = 1;
+contador++;
+console.log(contador);
+*/
+
+/* operadores de atribuição */
+let contador = 0;
+contador += 2;
+contador += 2;
+contador += 2;
+contador += 2;
+console.log(contador)
+
+
+/* converter uma string em número*/
+
+parseInt('5'); // inteiro 
+parseFloat('5.2'); // decimais
+Number('Luiz')//number
