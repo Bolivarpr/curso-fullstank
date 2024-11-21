8c1f039b84a5f916f130e29f49682b3d9ba92070
Marcelo <dennermarcelopereira@gmail.com>
2024-11-20 23:48:37 -0300

    caixas de alerta

---
+++ alert,confirmar e prompt/alerta.js
@@ -0,0 +1,12 @@
+/* função de caixa de alerta no navegador 
+alert('conseguiu!');
+
+confirm('quer cancelar?');
+
+prompt('digite seu nome');
+*/
+
+const num1 = prompt('digite um número:');
+const num2 = prompt('digite outro número:');
+let resultado = num1 * num2
+alert('o resultado da soma foi'+ resultado);
