96e4d2014c52f69aaf6b3fbfc4e05f5bc9fde60c
Marcelo <dennermarcelopereira@gmail.com>
2024-11-28 19:31:27 -0300

    Objeto Math

---
+++ objeto Math/math.js
@@ -0,0 +1,15 @@
+
+ Math.floor()  // arredonda para baixo ex: 9.568 fica 9
+
+ Math.ceil() // arredonda para cima ex: 9.12345 fica 10
+
+ Math.round() // retorna um número mais próximo ex: 9.49 fica 9 e 9.51 fica 10
+
+ Math.max() // para achar um número maior dentro dos parentes
+
+ Math.min() // para achar o menor número
+
+ Math.random() // para gerar número aleatório
+
+ console.log(Math.pow(2, 10));
+ console.log(2 ** 10);
