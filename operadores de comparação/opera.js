58cd5b829361f248c63c907ad50ebc54edbf26e1
Marcelo <dennermarcelopereira@gmail.com>
2024-12-17 20:03:44 -0300

    operadores de comparação

---
+++ operadores de comparação/opera.js
@@ -0,0 +1,17 @@
+/*
+operadores de comparação
+
+> - maior que
+>= - maior ou igual a
+< - menor que
+<= - menor ou igual a
+== - igualdade (valor)
+=== - igualdade estrita (valor e tipo)
+!= - diferente (valor)
+!== -  diferente estrito (valor e tipo)
+*/
+
+let num1 = 6;
+let num2 = 8;
+const resultado = num1 !== num2;
+console.log(resultado);
