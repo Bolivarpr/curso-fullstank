46f36ce521314e786eb23f79338a4243e29e8308
Marcelo <dennermarcelopereira@gmail.com>
2024-12-25 21:14:14 -0300

    operação ternária

---
+++ operação ternária/ternaria.js
@@ -0,0 +1,5 @@
+// (condição), (valor para verdadeiro)? (valor para falso):
+
+const Pontua = 100;
+const nivelUsuario = Pontua >= 200 ? 'usuario vip' : 'usuario normal';
+console.log(nivelUsuario);
