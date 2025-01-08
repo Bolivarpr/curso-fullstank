dce131e0fd70e4df28bb7990a399ff141845abfa
Marcelo <dennermarcelopereira@gmail.com>
2025-01-07 22:49:48 -0300

    estrutura de repetição usando for

---
+++ Lógica de programação/for/script.js
@@ -0,0 +1,10 @@
+
+for (let i = 0; i <= 5; i++) {
+
+    console.log(`linha ${i}`);
+}
+
+// O for é usado na programação para repetir um bloco de código várias vezes, 
+// geralmente quando se sabe antecipadamente o número de repetições necessárias. 
+// Ele é amplamente utilizado para percorrer sequências, como listas, tuplas, 
+// strings e intervalos numéricos.
