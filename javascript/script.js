b5b65b6b620f1d62dbf154094ec932cd0aca74cf
Marcelo <dennermarcelopereira@gmail.com>
2025-01-27 21:52:31 -0300

    while / do while estrutura de repetição

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,30 @@
-const nome = 'Luiz Otávio';
+// while e do while
+ /* let i = 1;
 
-/*for (let i = 0; i < nome.length; i++) {
-    console.log(nome[i])
-};
-*/
+ while (i <= 8) {
+    console.log(i);
+    i++;
+ }
+*/ 
 
-//for of -> retorna o valor em si (iteráveis, array ou strings)
 
-for (let valor of nome) {
-    console.log(valor);
-};
+function random(min, max) {
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r);
+}
 
+const min = 1;
+const max = 20;
+let rand =  10;
+
+while (rand !== 10); {
+ rand = random(min, max);
+ console.log(rand);
+}
+
+console.log('#####');
+
+ do {
+    rand = random(min, max);
+    console.log(rand);
+ } while(rand !== 10);
