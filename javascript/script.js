3a0b743f0c6cf6eff2931c5f671b00cc167bd900
Marcelo <dennermarcelopereira@gmail.com>
2025-01-31 22:00:57 -0300

    tratando erro e lançando com (try, catch, finally)

--- javascript/script.js
+++ javascript/script.js
@@ -3,28 +3,14 @@
 // throw - usar para por a mensagem de erro 
 
 try {
-   console.log(naoExisto);
-} catch(err) {
-   console.log('naoExite não existe .');
-}
-
-function soma(x, y) {
-   if (
-      typeof x !== 'number' ||
-       typeof y !== 'number'
-      ) {
-      throw('x e y precisam ser números.'); // O throw é utilizado para lançar um erro em JavaScript. 
-   }
-
-   return x + y;
-}
-
-// O bloco try...catch é usado para capturar e exibir esse erro de maneira controlada, sem interromper a execução do restante do código.
-// se não houver erro no try não ativa o cath mas se tiver erro irá ativar o try 
-
-try {
-console.log(soma(5,8));
-console.log(soma('1', 2));
-} catch(error) {
-   console.log('alguma coisa boa ao usuário'); // no cath pode arrumar o erro
+// executa quando não há erro
+console.log(a);
+console.log('abri um arquivo');
+console.log('manipulei um arquivo');
+} catch(i) {
+// executa quando há erro
+console.log('tratando');
+} finally {
+// sempre será executado
+console.log('sempre executado');
 }
